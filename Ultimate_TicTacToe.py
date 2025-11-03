ea20de93673c5267c9170b5e2b68e45e49db2518
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2025-11-03 14:10:56 +0530

    Update Ultimate_TicTacToe.py
    Added the __name__ == "__main__" part to make the code more professional.

--- Ultimate_TicTacToe.py
+++ Ultimate_TicTacToe.py
@@ -93,153 +93,158 @@ class Board:
                 return 'Playable Board'
 
 
-def update_big_board():
+def main():
+    def update_big_board():
     # This function updates both the Board object big_board for checking result
     # as well as the str playing_board for enhanced visibility
+    # nonlocal keyword used as the variables are inside main(), not global scope
 
-    global miniboard_tracker, big_board, playing_board
+        nonlocal miniboard_tracker, big_board, playing_board
 
-    for board_number, actual_board in miniboard_tracker.items():
-        result = actual_board.check_winner()
-        if result == 'X':
-            big_board.x.add(board_number)
-            playing_board = playing_board.replace(f"Board {board_number} ", X_WINNER_DECLARATION)
+        for board_number, actual_board in miniboard_tracker.items():
+            result = actual_board.check_winner()
+            if result == 'X':
+                big_board.x.add(board_number)
+                playing_board = playing_board.replace(f"Board {board_number} ", X_WINNER_DECLARATION)
+            
+            elif result == 'O':
+                big_board.o.add(board_number)
+                playing_board = playing_board.replace(f"Board {board_number} ", O_WINNER_DECLARATION)
+            
+            elif result == 'Draw':
+                big_board.draws.add(board_number)
+                playing_board = playing_board.replace(f"Board {board_number}", DRAW_DECLARATION)
+
+
+    def game_over():
+        nonlocal big_board
+        update_big_board()
+        if big_board.check_winner() != "Playable Board":
+            return True
         
-        elif result == 'O':
-            big_board.o.add(board_number)
-            playing_board = playing_board.replace(f"Board {board_number} ", O_WINNER_DECLARATION)
-        
-        elif result == 'Draw':
-            big_board.draws.add(board_number)
-            playing_board = playing_board.replace(f"Board {board_number}", DRAW_DECLARATION)
+        else:
+            return False
 
 
-def game_over():
-    global big_board
-    update_big_board()
-    if big_board.check_winner() != "Playable Board":
-        return True
-    
-    else:
-        return False
+    print("While inputting, player1 plays first, player2 goes next.")
+    PLAYER_X = input("Enter player1: ") # X
+    PLAYER_O = input("Enter player2: ") # O
+    print(f"{PLAYER_X}, you play X. {PLAYER_O}, you play O.")
 
+    playing_board = UTT_BOARD
+    big_board = Board()
 
-print("While inputting, player1 plays first, player2 goes next.")
-PLAYER_X = input("Enter player1: ") # X
-PLAYER_O = input("Enter player2: ") # O
-print(f"{PLAYER_X}, you play X. {PLAYER_O}, you play O.")
+    # ASCII values: a = 97 = 96 + 1 ; i = 105 = 96 + 9
+    # we want key-value pairs like {"a": 1, "b": 2, ..., "i": 9}
+    move_to_board_mapping = {chr(96 + i): i for i in range(1, 10)}
 
-playing_board = UTT_BOARD
-big_board = Board()
+    miniboard_tracker: dict[int, Board] = {}
+    for i in range(1, 10):
+        miniboard_tracker[i] = Board() # board number : board pairs
 
-# ASCII values: a = 97 = 96 + 1 ; i = 105 = 96 + 9
-# we want key-value pairs like {"a": 1, "b": 2, ..., "i": 9}
-move_to_board_mapping = {chr(96 + i): i for i in range(1, 10)}
-
-miniboard_tracker: dict[int, Board] = {}
-for i in range(1, 10):
-    miniboard_tracker[i] = Board() # board number : board pairs
-
-# There are no do-while loops in Python, so we put this outside the loop
-print(playing_board)
-x_move = input(f"{PLAYER_X}, enter the coordinate to play your piece: ").strip().lower()
-# strip -> removes whitespace added by user, if any
-# move[0] -> letter which indicates board, eg: "h6"[0] = "h" -> Board 8
-# move[1] -> integer (in string format)
-
-board_used: Board = miniboard_tracker[move_to_board_mapping[x_move[0]]]
-board_used.x.add(int(x_move[1]))
-
-board_num_redirected_to = int(x_move[1])
-playing_board = playing_board.replace(x_move, FORMATTED_X)
-print(playing_board)
-
-while True:
-    # O MOVE
-    if miniboard_tracker[board_num_redirected_to].check_winner() != 'Playable Board':
-        print(f"{PLAYER_O}, you may play in any board.")
-        random_move = True # player may play anywhere
-
-    else:
-        print(f"{PLAYER_O}, you must play in Board {board_num_redirected_to}.")
-        random_move = False # player is restricted to a board
-
-    o_move = input(f"{PLAYER_O}, enter the coordinate to play your piece: ").strip().lower() 
-
-    board_used_num = move_to_board_mapping[o_move[0]]
-    board_used: Board = miniboard_tracker[board_used_num]
-    
-    # ILLEGAL MOVE IDENFITICATION
-    if (not random_move) and (board_used_num != board_num_redirected_to):
-        raise ValueError(f"Board in play was Board {board_num_redirected_to}. Board played in was Board {board_used_num}.")
-
-    if (int(o_move[1]) in (board_used.x).union(board_used.o)):
-        # if coordinated has already been taken
-        raise ValueError(f"Coordinate {o_move} was already used in earlier moves.")
-
-    board_used.o.add(int(o_move[1])) # marking the coordinate in the board's relevant set
-    board_num_redirected_to = int(o_move[1])
-
-    playing_board = playing_board.replace(o_move, FORMATTED_O)
-
-    # CHECKING GAME OVER
-    if game_over():
-        result = big_board.check_winner()
-        print(playing_board) # reference to users to see why it's a win/draw
-        if result == "Draw":
-            print("Game drawn.")
-        
-        elif result == "X":
-            print(f"{PLAYER_X} won the game.")
-        
-        elif result == "O":
-            print(f"{PLAYER_O} won the game.")
-
-        break
-
-    # printing playing_board after checking game_over() as the function modifies
-    # the playing_board to enhance visibility if any board has a result
+    # There are no do-while loops in Python, so we put this outside the loop
     print(playing_board)
-
-    # X MOVE
-    if miniboard_tracker[board_num_redirected_to].check_winner() != 'Playable Board':
-        print(f"{PLAYER_X}, you may play in any board.")
-        random_move = True # player may play anywhere
-
-    else:
-        print(f"{PLAYER_X}, you must play in Board {board_num_redirected_to}.")
-        random_move = False # player is restricted to a board
-
     x_move = input(f"{PLAYER_X}, enter the coordinate to play your piece: ").strip().lower()
+    # strip -> removes whitespace added by user, if any
+    # move[0] -> letter which indicates board, eg: "h6"[0] = "h" -> Board 8
+    # move[1] -> integer (in string format)
 
-    board_used_num = move_to_board_mapping[x_move[0]]
-    board_used: Board = miniboard_tracker[board_used_num]
-
-    # ILLEGAL MOVE IDENITIFCATION
-    if (not random_move) and (board_used_num != board_num_redirected_to):
-        raise ValueError(f"Board in play was Board {board_num_redirected_to}. Board played in was Board {board_used_num}.")
-
-    if (int(x_move[1]) in (board_used.x).union(board_used.o)):
-        raise ValueError(f"Coordinate {x_move} was already used in earlier moves.")
-
+    board_used: Board = miniboard_tracker[move_to_board_mapping[x_move[0]]]
     board_used.x.add(int(x_move[1]))
+
     board_num_redirected_to = int(x_move[1])
-
     playing_board = playing_board.replace(x_move, FORMATTED_X)
-
-    # CHECKING GAME OVER
-    if game_over():
-        result = big_board.check_winner()
-        print(playing_board)
-        if result == "Draw":
-            print("Game drawn.")
-        
-        elif result == "X":
-            print(f"{PLAYER_X} won the game.")
-        
-        elif result == "O":
-            print(f"{PLAYER_O} won the game.")
-
-        break
-    
     print(playing_board)
+
+    while True:
+        # O MOVE
+        if miniboard_tracker[board_num_redirected_to].check_winner() != 'Playable Board':
+            print(f"{PLAYER_O}, you may play in any board.")
+            random_move = True # player may play anywhere
+
+        else:
+            print(f"{PLAYER_O}, you must play in Board {board_num_redirected_to}.")
+            random_move = False # player is restricted to a board
+
+        o_move = input(f"{PLAYER_O}, enter the coordinate to play your piece: ").strip().lower() 
+
+        board_used_num = move_to_board_mapping[o_move[0]]
+        board_used: Board = miniboard_tracker[board_used_num]
+        
+        # ILLEGAL MOVE IDENFITICATION
+        if (not random_move) and (board_used_num != board_num_redirected_to):
+            raise ValueError(f"Board in play was Board {board_num_redirected_to}. Board played in was Board {board_used_num}.")
+
+        if (int(o_move[1]) in (board_used.x).union(board_used.o)):
+            # if coordinated has already been taken
+            raise ValueError(f"Coordinate {o_move} was already used in earlier moves.")
+
+        board_used.o.add(int(o_move[1])) # marking the coordinate in the board's relevant set
+        board_num_redirected_to = int(o_move[1])
+
+        playing_board = playing_board.replace(o_move, FORMATTED_O)
+
+        # CHECKING GAME OVER
+        if game_over():
+            result = big_board.check_winner()
+            print(playing_board) # reference to users to see why it's a win/draw
+            if result == "Draw":
+                print("Game drawn.")
+            
+            elif result == "X":
+                print(f"{PLAYER_X} won the game.")
+            
+            elif result == "O":
+                print(f"{PLAYER_O} won the game.")
+
+            break
+
+        # printing playing_board after checking game_over() as the function modifies
+        # the playing_board to enhance visibility if any board has a result
+        print(playing_board)
+
+        # X MOVE
+        if miniboard_tracker[board_num_redirected_to].check_winner() != 'Playable Board':
+            print(f"{PLAYER_X}, you may play in any board.")
+            random_move = True # player may play anywhere
+
+        else:
+            print(f"{PLAYER_X}, you must play in Board {board_num_redirected_to}.")
+            random_move = False # player is restricted to a board
+
+        x_move = input(f"{PLAYER_X}, enter the coordinate to play your piece: ").strip().lower()
+
+        board_used_num = move_to_board_mapping[x_move[0]]
+        board_used: Board = miniboard_tracker[board_used_num]
+
+        # ILLEGAL MOVE IDENITIFCATION
+        if (not random_move) and (board_used_num != board_num_redirected_to):
+            raise ValueError(f"Board in play was Board {board_num_redirected_to}. Board played in was Board {board_used_num}.")
+
+        if (int(x_move[1]) in (board_used.x).union(board_used.o)):
+            raise ValueError(f"Coordinate {x_move} was already used in earlier moves.")
+
+        board_used.x.add(int(x_move[1]))
+        board_num_redirected_to = int(x_move[1])
+
+        playing_board = playing_board.replace(x_move, FORMATTED_X)
+
+        # CHECKING GAME OVER
+        if game_over():
+            result = big_board.check_winner()
+            print(playing_board)
+            if result == "Draw":
+                print("Game drawn.")
+            
+            elif result == "X":
+                print(f"{PLAYER_X} won the game.")
+            
+            elif result == "O":
+                print(f"{PLAYER_O} won the game.")
+
+            break
+        
+        print(playing_board)
+
+if __name__ == "__main__":
+    main()
